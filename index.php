21d3436f0448dc407946104614df50f88e8db1ac
Jasmin Gubeljic <jasmin.g@tutanota.com>
2025-01-28 08:46:12 +0100

    feat: initial landing page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jasmin Gubeljic - Task</title>
+</head>
+<body>
+    <h1>Statistika rezervacija</h1>
+    <h2>Prosječno trajanje rezervacija (u danima):</h2>
+    <p>99</p>
+    
+    <h2>Top 5 mjesta (po duljini trajanja rezervacije):</h2>
+    <p>99</p>
+
+    <h2>Top 5 rivijera (po duljini trajanja rezervacije):</h2>
+    <p>99</p>
+
+    <h2>Ukupan prihod od rezervacija po godini:</h2>
+    <p>99</p>
+
+    <h2>Lista gostiju koji su rezervirali više od jednom (poredanih od najvećeg broja ostvarenih
+    rezervacija prema najmanjem):</h2>
+    <p>99</p>
+</body>
+</html>
